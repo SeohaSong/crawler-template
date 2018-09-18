44d816b6035a2105dd9740d34465549a8ff3345d
seohasong <tisutoo@gmail.com>
2018-09-19 03:48:19 +0900

    update origin master

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 from module.utils.preprocessor import get_sentences
 from module.utils.text_collection import get_korea_sat_nonliteral
 from module.utils.crawler_collection import get_naver_web_novel
+from module.utils.crawler_collection import Typing_DNA
 from module.dic_converter import convert2xml
 
 
@@ -59,35 +60,59 @@ if __name__ == "__main__":
     # 구르미 그린 달빛 - 윤이수 (무료 공개본)
     # 르네 마그리트의 연인 - 유지나 (무료 공개본)
 
-    args = [
-        {"novel_id": id_, "volume_no": no+1}
-        for id_, max_no in [
-            (126772, 7),
-            (494460, 9),
-        ]
-        for no in range(max_no)
-    ]
+    # args = [
+    #     {"novel_id": id_, "volume_no": no+1}
+    #     for id_, max_no in [
+    #         (126772, 7),
+    #         (494460, 9),
+    #     ]
+    #     for no in range(max_no)
+    # ]
+
+    # i = 0
+    # total_ko = []
+    # while True:
+    #     try:
+    #         if not i:
+    #             docs = get_korea_sat_nonliteral()
+    #         else:
+    #             docs = get_naver_web_novel(**args[i-1])
+    #         i += 1
+    #     except:
+    #         continue
+    #     sens = sum([get_sentences(doc, "ko", [160, 200]) for doc in docs], [])
+    #     total_ko += sens
+    #     total_len = len(set(total_ko))
+    #     sys.stdout.write("\r% 4d | % 4d" % (i, total_len))
+    #     if total_len >= 750:
+    #         total_ko = list(set(total_ko))[:750]
+    #         pd.to_pickle(total_ko, "./data/key-ko")
+    #         print()
+    #         break
+
 
     i = 0
-    total_ko = []
+    total_en_sens = []
+    site = Typing_DNA()
     while True:
         try:
-            if not i:
-                docs = get_korea_sat_nonliteral()
-            else:
-                docs = get_naver_web_novel(**args[i-1])
+            docs = site.crawl()
             i += 1
         except:
             continue
-        sens = sum([get_sentences(doc, "ko", [160, 180]) for doc in docs], [])
-        total_ko += sens
-        total_len = len(set(total_ko))
+        sens = sum([get_sentences(doc, "en", [160, 200]) for doc in docs], [])
+        total_en_sens += sens
+        total_len = len(set(total_en_sens))
         sys.stdout.write("\r% 4d | % 4d" % (i, total_len))
-        if total_len >= 500:
-            total_ko = list(set(total_ko))[:500]
-            pd.to_pickle(total_ko, "./data/key-ko")
+        
+        if total_len >= 3:
+            total_en_sens = list(set(total_en_sens))[:3]
+            pd.to_pickle(total_en_sens, "./data/key-en")
             print()
             break
 
-    ids = [str(i+1) for i in range(50)]
+
+    ids = [str(i+1) for i in range(4)]
     set_test(ids)
+
+
